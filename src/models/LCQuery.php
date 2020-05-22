e949dbdd54950d218301864fda8448587d986bc9
Pozitronik <antikillerxxl@gmail.com>
2020-05-22 22:01:26 +0300

    Re-introduce LCQuery (for re-exporting in find())

---
+++ src/models/LCQuery.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace models;
+
+use pozitronik\core\traits\ActiveQueryExtended;
+use yii\db\ActiveQuery;
+
+/**
+ * Class LCQuery (назван так в честь LightCab, в котором такое решение впервые появилось)
+ */
+class LCQuery extends ActiveQuery {
+	use ActiveQueryExtended;
+}
