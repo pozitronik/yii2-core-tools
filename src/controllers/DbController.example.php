77c02347e1e603b6ab678bc07c88e94f7cecb78d
Pozitronik <antikillerxxl@gmail.com>
2021-03-22 17:39:57 +0300

    Controller and view examples added

---
+++ src/controllers/DbController.example.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+/*todo: add namespace here*/
+
+use pozitronik\core\models\DbMonitor;
+use yii\data\ArrayDataProvider;
+use yii\web\Controller;
+use yii\web\Response;
+
+/**
+ * Class DbController
+ */
+class DbController extends Controller {
+
+	/**
+	 * Список процессов на базе данных
+	 * @return string
+	 */
+	public function actionProcessList() {
+		$provider = new ArrayDataProvider([
+			'allModels' => DbMonitor::ProcessList(),
+			'sort' => [
+				'attributes' => ['id', 'user_id', 'time'],
+				'defaultOrder' => ['time' => SORT_DESC]
+			]
+		]);
+		return $this->render('process-list', [
+			'dataProvider' => $provider,
+			'message' => Yii::$app->session->getFlash('DbMonitorMessage', false, true)
+		]);
+	}
+
+	/**
+	 * @param int $process_id
+	 * @return Response
+	 */
+	public function actionKill(int $process_id):Response {
+		Yii::$app->session->setFlash('DbMonitorMessage', (null === $affected = DbMonitor::kill($process_id))?"None killed":"{$affected} row(s) $affected");
+		return $this->redirect(['process-list']);
+	}
+}
